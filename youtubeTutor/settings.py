ff8e14893b847f3e1c157a9bb23bc08cee7fad56
Aaronphilip2003 <aaronphilip2003@gmail.com>
2023-11-03 17:55:23 +0530

    Added vercel connection

--- youtubeTutor/settings.py
+++ youtubeTutor/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#6vfvgu_z-=$i#$hg4@yr9i8sttq)h=i0wq%fy=ub=^5r6&722
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','.vercel.app']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'query'
 ]
 
 MIDDLEWARE = [
