f3140fc96c681c6c236480861e0d60af2590743b
Aaronphilip2003 <aaronphilip2003@gmail.com>
2023-11-21 12:13:12 +0530

    training and responses done

--- query/views.py
+++ query/views.py
@@ -16,9 +16,12 @@ from langchain.llms import HuggingFacePipeline
 from langchain.chains.question_answering import load_qa_chain
 import json
 from langchain.llms.huggingface_hub import HuggingFaceHub
+from django.views.decorators.csrf import csrf_exempt
+
 
 # Function to extract video id from url
 def extract_video_id(url):
+    print("This is the url",url)
     # Parse URL
     url_data = urlparse.urlparse(url)
     # Extract video id
@@ -39,7 +42,12 @@ def yt(request):
     query = request.GET.get('query', '')
     ans = vdb_chunks_HF.as_retriever().get_relevant_documents(query)
     answers = [doc.page_content for doc in ans]
-    return JsonResponse({'answers': answers})
+
+    # Add CORS headers to the response
+    response = JsonResponse({'answers': answers})
+    response['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+    return response
+
 
 # def llm_answering(request):
 #     url = request.GET.get('url', '')
@@ -55,10 +63,28 @@ def yt(request):
 #     response = chain.run(input_documents=docs, question=query)
 #     return response
 
+# def llm_answering(request):
+#     # Assuming you're using Django for web development
+#     url = request.GET.get('url', '')
+#     print(url)
+#     query = request.GET.get('query', '')
+#     # Validate if both 'url' and 'query' parameters are present
+#     if not url or not query:
+#         return HttpResponse("Both 'url' and 'query' parameters are required.")
+#     video_id = extract_video_id(url)
+#     embedding2 = GooglePalmEmbeddings(google_api_key="AIzaSyBysL_SjXQkJ8lI1WPTz4VwyH6fxHijGUE")
+#     # Adjust the path to your FAISS index directory
+#     db = FAISS.load_local("./query/vdb_chunks_HF/", embedding2, index_name=f"index{video_id}")
+#     llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature": 0.1, "max_length": 65536, "min_length": 32768}, huggingfacehub_api_token="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")
+#     chain = load_qa_chain(llm, chain_type="stuff")
+#     print(f"Query: {query}")
+#     docs = db.similarity_search(query)
+#     response = chain.run(input_documents=docs, question=query)
+#     return JsonResponse({'response': response})
+
 def llm_answering(request):
     # Assuming you're using Django for web development
     url = request.GET.get('url', '')
-    print(url)
     query = request.GET.get('query', '')
     # Validate if both 'url' and 'query' parameters are present
     if not url or not query:
@@ -69,17 +95,20 @@ def llm_answering(request):
     db = FAISS.load_local("./query/vdb_chunks_HF/", embedding2, index_name=f"index{video_id}")
     llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature": 0.1, "max_length": 65536, "min_length": 32768}, huggingfacehub_api_token="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")
     chain = load_qa_chain(llm, chain_type="stuff")
-    print(f"Query: {query}")
     docs = db.similarity_search(query)
     response = chain.run(input_documents=docs, question=query)
-    return JsonResponse({'response': response})
+
+    # Add CORS headers to the response
+    response = JsonResponse({'response': response})
+    response['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+    return response
 
 
 def process_youtube_video(request):
     print(request)
     # Get YouTube URL from request parameters
-    url = request.GET.get('url', '')
-
+    url = request.GET.get('query', '')
+    print("This is the url",request.GET.get('query', ''))
     # Extract video id
     video_id = extract_video_id(url)
 
@@ -112,10 +141,10 @@ def process_youtube_video(request):
     embedding = GooglePalmEmbeddings(google_api_key="AIzaSyBysL_SjXQkJ8lI1WPTz4VwyH6fxHijGUE")
     vdb_chunks_HF = FAISS.from_documents(docs, embedding=embedding)
     vdb_chunks_HF.save_local(f'./query/vdb_chunks_HF/', index_name=f"index{video_id}")
-
     return JsonResponse({'status': 'success'})
 
 # http://127.0.0.1:8000/query/yt/?query=omega&url=https://www.youtube.com/watch?v=7kcWV6zlcRU&list=PLUl4u3cNGP62esZEwffjMAsEMW_YArxYC&index=5&ab_channel=MITOpenCourseWare
 # http://127.0.0.1:8000/query/ytvid/?url=https://www.youtube.com/watch?v=IcmzF1GT1Qw
 # http://127.0.0.1:8000/query/llm/?query=what+is+omega&url=https://www.youtube.com/watch?v=7kcWV6zlcRU&list=PLUl4u3cNGP62esZEwffjMAsEMW_YArxYC&index=5&ab_channel=MITOpenCourseWare
 # http://127.0.0.1:8000/query/llm/?query=what+does+he+say+about+beethoven+?&url=https://www.youtube.com/watch?v=IcmzF1GT1Qw&ab_channel=Vienna
+# https://www.youtube.com/watch?v=Tuw8hxrFBH8
